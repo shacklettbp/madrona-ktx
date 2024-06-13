4a8c9f7e2a652022a4ecbfa6dbde8f9b3518de29
WarrenXiaGG <rlaterson@gmail.com>
2024-06-13 12:54:48 -0400

    Initial commit

---
+++ madrona_ktx.cpp
@@ -0,0 +1,7 @@
+#include "madrona_ktx.h"
+
+#include <iostream>
+
+void hello() {
+    std::cout << "Hello, World!" << std::endl;
+}
---
+++ madrona_ktx.h
@@ -0,0 +1,6 @@
+#ifndef MADRONA_KTX_MADRONA_KTX_H
+#define MADRONA_KTX_MADRONA_KTX_H
+
+void hello();
+
+#endif //MADRONA_KTX_MADRONA_KTX_H
